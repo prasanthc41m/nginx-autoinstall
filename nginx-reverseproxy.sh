73ddb4e1a3d46765af88bc05a964b7d64735ea64
Prasanthc41m <58906808+prasanthc41m@users.noreply.github.com>
2020-08-31 00:45:20 +0530

    Update nginx-reverseproxy.sh

--- nginx-reverseproxy.sh
+++ nginx-reverseproxy.sh
@@ -6,7 +6,9 @@ read -p 'Enter domain name: ' domain
 read -p 'Enter server port number: ' port
 apt-get update && apt -y install nginx
 systemctl enable nginx.service && systemctl start nginx.service
-tail -13 /etc/nginx/sites-enabled/default > $domain
+cp /etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/original
+tail -13 /etc/nginx/sites-enabled/original > $domain
+#tail -13 /etc/nginx/sites-enabled/default > $domain
 mv $domain /tmp/file.txt
 cd /tmp
 sed -i 's/#/ /g' file.txt
