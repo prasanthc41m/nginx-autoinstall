ba1d654358540ab3160d149d195c4f13ffc67f30
Prasanthc41m <58906808+prasanthc41m@users.noreply.github.com>
2020-09-03 17:58:21 +0530

    Update nginx-vhost.sh

--- nginx-vhost.sh
+++ nginx-vhost.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #
-#Nginx and LetsEncrypt
+##Nginx and LetsEncrypt
 #
 read -p 'Enter Domain name: ' domain
-#apt-get update && apt -y install nginx
+apt-get update && apt -y install nginx
 mkdir /var/www/$domain
 cd /var/www/$domain
 echo "<!DOCTYPE html><html><body><h1>Success!! $domain is working</h1></body></html>" > index.html
 cat < EOF | tee -a /etc/nginx/sites-enabled/change.com
-      ##Virtual Host
+##Virtual Host
 	server {
         listen 80;
         listen [::]:80;
