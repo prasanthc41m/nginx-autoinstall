e720d10f9ffa527619d9cc2737edf32af7e6d042
Prasanthc41m <58906808+prasanthc41m@users.noreply.github.com>
2020-09-03 17:56:47 +0530

    Update nginx-reverseproxy.sh

--- nginx-reverseproxy.sh
+++ nginx-reverseproxy.sh
@@ -26,12 +26,12 @@ cat < EOF | tee -a /etc/nginx/sites-enabled/change.com
 EOF
 mv $domain /tmp/file.txt
 cd /tmp
-sed -i 's/#/ /g' file.txt
-sed -i '7d;8d' file.txt
+#sed -i 's/#/ /g' file.txt
+#sed -i '7d;8d' file.txt
 sed -i "s/example.com/$domain/g" file.txt
 sed -i 's/try_files $uri $uri\/ =404/proxy_pass http:\/\/127.0.0.1:port/g' file.txt
 sed -i "s/port/$port/g" file.txt
-apt install -y python3-certbot-nginx && certbot --nginx 
 cat file.txt
 mv file.txt /etc/nginx/sites-enabled/$domain
-/etc/init.d/nginx restart
+apt install -y python3-certbot-nginx && certbot --nginx 
+service nginx restart
