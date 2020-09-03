1ac474ccf9425672799c45c3696f71f6c6acab9c
Prasanthc41m <58906808+prasanthc41m@users.noreply.github.com>
2020-09-03 17:53:18 +0530

    Update nginx-reverseproxy.sh

--- nginx-reverseproxy.sh
+++ nginx-reverseproxy.sh
@@ -7,8 +7,23 @@ read -p 'Enter server port number: ' port
 apt-get update && apt -y install nginx
 systemctl enable nginx.service && systemctl start nginx.service
 cp /etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/original
-tail -13 /etc/nginx/sites-enabled/original > $domain
-#tail -13 /etc/nginx/sites-enabled/default > $domain
+#tail -13 /etc/nginx/sites-enabled/original > $domain
+cat < EOF | tee -a /etc/nginx/sites-enabled/change.com
+      ##Virtual Host
+	server {
+        listen 80;
+        listen [::]:80;
+
+        root /var/www/example.com/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name example.com www.example.com;
+
+        location / {
+                try_files $uri $uri/ =404;
+        }
+}
+EOF
 mv $domain /tmp/file.txt
 cd /tmp
 sed -i 's/#/ /g' file.txt
