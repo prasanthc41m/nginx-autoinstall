f02fc4c7d254a11f0b7cb4e05a900a4a64151297
Prasanthc41m <58906808+prasanthc41m@users.noreply.github.com>
2020-09-09 21:22:22 +0530

    Update nginx-reverseproxy.sh

--- nginx-reverseproxy.sh
+++ nginx-reverseproxy.sh
@@ -3,15 +3,12 @@
 #Nginx reverse proxy and LetsEncrypt
 #
 read -p 'Enter domain name: ' domain
-
 apt-get update && apt -y install nginx nginx-full nginx-common
 systemctl enable nginx.service && systemctl start nginx.service
-curl https://raw.githubusercontent.com/prasanthc41m/nginx-install/master/default | head -n 13 > /tmp/$domain
-mv /tmp/$domain /tmp/file.txt
+curl https://raw.githubusercontent.com/prasanthc41m/nginx-install/master/default-rp > /tmp/file.txt
 cd /tmp
 sed -i "s/example.com/$domain/g" file.txt
 read -p 'Enter server port number: ' port
-sed -i "s/try_files $uri $uri\/ =404/proxy_pass http:\/\/127.0.0.1:port/g" file.txt
 sed -i "s/port/$port/g" file.txt
 cat file.txt
 mv file.txt /etc/nginx/sites-enabled/$domain
