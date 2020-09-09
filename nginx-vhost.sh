0900dfcf38f2cde91d9a4dc1bcf5ccd2eaea5480
Prasanthc41m <58906808+prasanthc41m@users.noreply.github.com>
2020-09-09 14:56:59 +0530

    Update nginx-vhost.sh

--- nginx-vhost.sh
+++ nginx-vhost.sh
@@ -7,9 +7,8 @@ apt-get update && apt -y install nginx nginx-full nginx-common
 mkdir /var/www/$domain
 cd /var/www/$domain
 echo "<!DOCTYPE html><html><body><h1>Success!! $domain is working</h1></body></html>" > index.html
-cat | curl https://raw.githubusercontent.com/prasanthc41m/nginx-install/master/default > /etc/nginx/sites-enabled/change.com
+curl https://raw.githubusercontent.com/prasanthc41m/nginx-install/master/default > /etc/nginx/sites-enabled/change.com
 cd /etc/nginx/sites-enabled/
-#sed -i 's/#/ /g' change.com
 sed -i "s/example.com/$domain/g" change.com
 mv change.com $domain
 apt install -y python3-certbot-nginx && certbot --nginx 
