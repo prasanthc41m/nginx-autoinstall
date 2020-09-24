95ad9bfcd4904a20f8ee980279c860c155c167c8
Prasanthc41m <58906808+prasanthc41m@users.noreply.github.com>
2020-09-24 09:42:44 +0530

    Add files via upload

---
+++ nginx-remove.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+service nginx stop
+apt-get remove --purge nginx nginx-full nginx-common -y
+rm -rf /etc/nginx
+rm /usr/sbin/nginx
+apt-get remove python-certbot-nginx -y
+apt-get purge --auto-remove python-certbot-nginx -y
+apt-get remove  python3-certbot-nginx -y 
+apt-get purge --auto-remove python3-certbot-nginx -y
