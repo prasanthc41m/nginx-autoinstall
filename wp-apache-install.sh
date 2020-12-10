2d93b937aae26941b8d5bd9eb3c8d0049ad534c7
Prasanthc41m <58906808+prasanthc41m@users.noreply.github.com>
2020-12-10 23:20:21 +0530

    Update wp-apache-install.sh

--- wp-apache-install.sh
+++ wp-apache-install.sh
@@ -4,14 +4,15 @@ apt install tasksel -y
 sudo tasksel install lamp-server
 
 read -p 'Enter Domain name: ' domain
+read -p 'Enter 1st part of Domain name eg www.example: ' 1domain
+read -p 'Enter last part of Domain name eg .com: ' 2domain
 mkdir -p /var/www/html/$domain/public_html
 chown -R www-data:www-data /var/www/html/$domain/
 curl https://raw.githubusercontent.com/prasanthc41m/nginx-autoinstall/master/example.conf > /etc/apache2/sites-enabled/change.conf
 cd /etc/apache2/sites-enabled/
 sed -i "s/example.com/$domain/g" change.conf
-read -p 'Enter 1st part of Domain name eg www.example: ' 1domain
-read -p 'Enter last part of Domain name eg .com: ' 2domain
-sed -i "s/example\.com/$1domain\.2domain/g" change.conf
+sed -i "s/com/.2domain/g" change.conf
+sed -i "s/example/$1domain/g" change.conf
 mv change.conf $domain
 sudo a2ensite $domain.conf
 sudo apache2ctl -M
