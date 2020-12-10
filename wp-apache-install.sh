760a0e0894b63befc0a42fa4a63e86b101f230ae
Prasanthc41m <58906808+prasanthc41m@users.noreply.github.com>
2020-12-10 23:06:16 +0530

    Update wp-apache-install.sh

--- wp-apache-install.sh
+++ wp-apache-install.sh
@@ -2,8 +2,22 @@
 apt-get update && apt-get upgrade
 apt install tasksel -y
 sudo tasksel install lamp-server
+
 read -p 'Enter Domain name: ' domain
 mkdir -p /var/www/html/$domain/public_html
+chown -R www-data:www-data /var/www/html/$domain/
+curl https://raw.githubusercontent.com/prasanthc41m/nginx-autoinstall/master/example.conf > /etc/apache2/sites-enabled/change.conf
+cd /etc/apache2/sites-enabled/
+sed -i "s/example.com/$domain/g" change.com
+read -p 'Enter 1st part of Domain name eg www.example: ' 1domain
+read -p 'Enter last part of Domain name eg .com: ' 2domain
+sed -i "s/example\.com/$1domain\.2domain/g" change.com
+mv change.com $domain
+sudo a2ensite $domain.conf
+sudo apache2ctl -M
+sudo a2enmod rewrite
+sudo systemctl reload apache2
+wget https://raw.githubusercontent.com/prasanthc41m/sitemaintenance/main/index.html
 
 # Create MySQL database
 
@@ -24,6 +38,7 @@ echo "Privileges granted...\n"
 echo "FLUSH PRIVILEGES;" | mysql -u root -p$rootpass
 echo "New MySQL database is successfully created"
 fi
+wget -O	/var/www/html/$domain/public_html/db.php http://www.adminer.org/latest.php
 
 #Install wordpress
 
@@ -31,7 +46,7 @@ read -p "Do you want to Install Wordpress? (y/n) " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-
+rm /var/www/html/$domain/public_html/index*
 cd /tmp/
 wget http://wordpress.org/latest.tar.gz
 tar -zxvf latest.tar.gz
@@ -47,20 +62,6 @@ curl https://api.wordpress.org/secret-key/1.1/salt/ >> wp-config.php
 grep -A 50 -B 3 'Table prefix' wp-config-sample.php >> wp-config.php
 sed -i "s/database_name_here/$dbname/;s/username_here/$dbuser/;s/password_here/$userpass/" wp-config.php
 fi
-
-chown -R www-data:www-data /var/www/html/$domain/
-curl https://raw.githubusercontent.com/prasanthc41m/nginx-autoinstall/master/example.conf > /etc/apache2/sites-enabled/change.conf
-cd /etc/apache2/sites-enabled/
-sed -i "s/example.com/$domain/g" change.com
-read -p 'Enter 1st part of Domain name eg www.example: ' 1domain
-read -p 'Enter last part of Domain name eg .com: ' 2domain
-sed -i "s/example\.com/$1domain\.2domain/g" change.com
-mv change.com $domain
-sudo a2ensite $domain.conf
-sudo apache2ctl -M
-sudo a2enmod rewrite
-sudo systemctl reload apache2
-wget -O	/var/www/html/$domain/public_html/db.php http://www.adminer.org/latest.php
 
 # Install LetsEncrypt
 
